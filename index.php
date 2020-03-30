00eb493f9f95499f09f8ba81c6778ec62be2427e
OungKoungMyut <48884468+OungKoungMyut@users.noreply.github.com>
2020-03-30 20:40:36 +0630

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include_once('admin_layout/header.php'); ?>
+<?php include_once('../database.php'); ?>
+<?php include_once('admin_layout/navigation.php'); ?>
+
+<?php include_once('admin_layout/view_songs.php'); ?>
+
+	
+
+
+<?php include_once('admin_layout/footer.php'); ?>
